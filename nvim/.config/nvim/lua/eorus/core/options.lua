858472433eab3e5aff2c881825f5263406c1724c
Alper Orus <alperor@gmail.com>
2025-01-09 10:36:38 +0300

    qutebrowser

--- nvim/.config/nvim/lua/eorus/core/options.lua
+++ nvim/.config/nvim/lua/eorus/core/options.lua
@@ -13,7 +13,7 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
-opt.wrap = false
+opt.wrap = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
