0bd4b8d4a899e431e21aec39b82e81a9a7bc09f3
Alper Orus <alperor@gmail.com>
2025-01-19 18:25:52 +0300

    upd

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -482,6 +482,23 @@ formatting = {
   },
 
   {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = "markdown",
+    lazy = true,
+    keys = { { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
+    config = function()
+      vim.g.mkdp_auto_close = true
+      vim.g.mkdp_open_to_the_world = false
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = "8888"
+      vim.g.mkdp_browser = "brave"
+      vim.g.mkdp_echo_preview_url = true
+      vim.g.mkdp_page_title = "${name}"
+    end,
+  },
+
+  {
 
     "https://github.com/mbbill/undotree",
     event = "VeryLazy",
